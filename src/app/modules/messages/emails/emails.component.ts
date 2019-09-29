bb238e63af1370533557a656ab36296401dd6f9f
shanaka <madushanka991@gmail.com>
2019-09-29 15:14:48 +0530

    email module changes

--- src/app/modules/messages/emails/emails.component.ts
+++ src/app/modules/messages/emails/emails.component.ts
@@ -8,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class EmailsComponent implements OnInit {
 
   threadQ: any[] = [
-    {id: 1, recipient: 'df', recImg: '', emailSubject: 'Pending subscription payment', dateTime: '1m ago',  viewStatus: ''},
-    {id: 1, recipient: 'df', recImg: '', emailSubject: 'New features', dateTime: '3h ago',  viewStatus: ''},
-    {id: 1, recipient: 'dfdfdf', recImg: '', emailSubject: 'Congrtulations, You have won ', dateTime: '1 day ago',  viewStatus: ''},
-    {id: 1, recipient: 'fgfgfg', recImg: '', emailSubject: 'Verify your email address', dateTime: '12:25:00 AM 12/12/2019',  viewStatus: ''},
+    {id: 1, recipient: 'df', recImg: '', emailSubject: 'Pending subscription payment', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' , dateTime: '1m ago',  viewStatus: false},
+    {id: 1, recipient: 'df', recImg: '', emailSubject: 'New features', dateTime: '3h ago', body: 'Lorem Ipsum is simply dummy text of the' ,  viewStatus: false},
+    {id: 1, recipient: 'dfdfdf', recImg: '', emailSubject: 'Congrtulations, You have won ', dateTime: 'Yesterday', body: 'Lorem Ipsum is simply dummy text of the' ,  viewStatus: false},
+    {id: 1, recipient: 'fgfgfg', recImg: '', emailSubject: 'Verify your email address', dateTime: '12:25:00 AM 12/12/2019', body: 'Lorem Ipsum is simply dummy text of the' ,  viewStatus: true},
   ];
+
+  selectedEmail: any = {
+    from: 'Cambio Careers <careers@cambio.lk>',
+    to: 'Shanaka Madushanka <madushanka991@gmail.com>',
+    subject: 'Application for position of Software Engineer',
+    body: ''
+  };
 
   constructor() { }
 
